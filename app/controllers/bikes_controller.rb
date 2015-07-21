0ab26f4a5d8f75d16fe03979a6d3a75d9a5d95fe
sethherr <seth.william.herr@gmail.com>
2015-07-21 08:36:37 -0500

    Handle get request to bikes create
    For some reason, it's interpreting post create requests as gets. So send those to create too.

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -12,6 +12,9 @@ class BikesController < ApplicationController
     render json: { success: true, tweet: new_tweet }
   end
 
+  def index
+  end
+
   private
   def verify_key
     unless params[:key].present? && params[:key] == ENV['INCOMING_REQUEST_KEY']
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
-  post 'bikes/create'
+
+  resources :bikes, only: [:create, :index]
 
   root 'welcome#index'
 
